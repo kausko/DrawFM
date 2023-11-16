dfaf2806305afe06aa84822878126a055ee4ef41
Alexander Puckhaber <alexanderpuckhaber@gatech.edu>
2023-11-16 17:39:54 -0500

    struct packing

--- sim_rx_drawing.py
+++ sim_rx_drawing.py
@@ -3,8 +3,20 @@ import random
 from comm_simulator import CommunicationSimulator
 from multiprocessing import shared_memory
 from time import sleep
+from struct import *
+import time
+
+from si4703Library import si4703Radio
 
 def rx_drawing(shared_input_buffer_name: str, communications_simulator: CommunicationSimulator):
+
+    # IF USING HARDWARE, USE THIS
+    radio = si4703Radio(0x10, 5, 19)
+    radio.si4703Init()
+    radio.si4703SetChannel(877)
+    radio.si4703SetVolume(5)
+    print(str(radio.si4703GetChannel()))
+    print(str(radio.si4703GetVolume()))
 
     # pygame setup
     pygame.init()
@@ -36,49 +48,106 @@ def rx_drawing(shared_input_buffer_name: str, communications_simulator: Communic
             if mock:
                 mock_draw()
             else:
-                try:
-                    rds = "100 100"
-                    # rds = communications_simulator.get_buffer().decode()
-                    # rds = str(communications_simulator.in_buffer.decode())
-                    s2 = shared_memory.ShareableList(name=shared_input_buffer_name)
-                    tx_buffer = s2[0]
-                    # tx_buffer = tx_buffer.encode('utf-8')
-                    communications_simulator.set_buffer(tx_buffer)
-                    rds = communications_simulator.get_buffer()
-                    print('rds:', rds)
-                    # rds is a stringified tuple (x, y), destructure it
-                    # stripped = rds[1:-1].split(", ")
-                    
-                    try:
-                        rds = rds.decode()
-                        stripped = rds.rstrip("\x00").split(" ")
-                        if len(stripped) == 1:
-                            if stripped[0] == "clean":
-                                # clear canvas or something
-                                screen.fill((0, 0, 0))
-                                coords = []
-                        else:
-                            x, y = int(stripped[0]), int(stripped[1])
-                            coords.append([x, y])
-                            if len(coords) > 1:
-                                if len(stripped) > 2:
-                                    if stripped[2] == 'd':
-                                        pygame.draw.lines(screen, (255, 255, 255), False, coords)
-                                    else:
-                                        pygame.draw.ellipse(screen, (255, 255, 255), pygame.Rect(x, y, 4, 4), 2)
-                                
-                                coords.pop(0)
+                # try:
+                # rds = "100 100"
 
-                    except Exception as e:
-                        print(e)
-                        print("RDS:", rds, "is not a valid coordinate pair")
-                        pass
+                # IF SIMULATOR MODE, USE THIS
+                # rds = str(communications_simulator.in_buffer.decode())
+                # s2 = shared_memory.ShareableList(name=shared_input_buffer_name)
+                # tx_buffer = s2[0]
+                # communications_simulator.set_buffer(tx_buffer)
+                # rds = communications_simulator.get_buffer()
+
+                # IF USING HARDWARE, USE THIS
+                # rds = radio.si4703getRDSBytes()
+
+                # rds is a stringified tuple (x, y), destructure it
+                # stripped = rds[1:-1].split(", ")
+                
+                try:
+                    # rds = radio.si4703getRDS()
+                    rds = radio.si4703getRDSBytes()
+
+
+
+                    print('rds:', rds)
+                    # rds = rds.decode()
+
+                    x = 0
+                    y = 0
+                    data_line = 0
+                    last_data_line = data_line
+                    data_clean = 0
+
+                    # invalid = 1
+                    invalid = 0
+
+                    # print(rds)
+                    # print(bytes(rds, 'utf-8'))
+                    # have to select first number of bytes we want, becuse rds reader will give us extra zero bytes at end because
+                    (x, y, data_line, data_clean) = unpack('>hhBB', rds[:6])
+
+                    print(x, y, data_line, data_clean, time.time())
+
+                    # x += 128
+
+                    # stripped = rds.rstrip("\x00").split(" ")
+                   
+                    
+                    # if len(stripped) == 1:
+                    #     if stripped[0] == "clear":
+                    #         data_clean = 1
+                    #         # clear canvas or something
+                    #         screen.fill((0, 0, 0))
+                    #         coords = []
+                    # else:
+                    #     x, y = int(stripped[0]), int(stripped[1])
+                    #     coords.append([x, y])
+
+                    # if len(stripped) == 3:
+                    #     if stripped[2] == 'd':
+                    #         data_line = 1
+                    #         invalid = 0
+                    #     elif stripped[2] == 'p':
+                    #         invalid = 0
+                    
+                    print(x, y, data_line, data_clean, time.time())
+
+                    coords.append([x, y])
+                    
+                    if data_clean == 255:
+                        # clear canvas or something
+                        screen.fill((0, 0, 0))
+                        coords = []
+                    elif True:
+                        # print(coords)
+                        if data_line == 255 or data_line == 0:
+                            if data_line != last_data_line:
+                                # pygame.draw.lines(screen, (255, 255, 255), False, coords[:-1])
+                                coords = [coords[-1]]
+                                last_data_line = data_line
+                            else:
+                                pygame.draw.lines(screen, (255, 255, 255), False, coords)
+                            # pygame.draw.lines(screen, (255, 255, 255), False, coords[:-2])
+                            
+
+                            # coords = coords[-1]
+                        # else:
+                            pygame.draw.ellipse(screen, (255, 255, 255), pygame.Rect(x, y, 4, 4), 2)
+                            
+                        #     # coords.pop(0)
+                        #     coords = [[x, y]]
+
                 except Exception as e:
                     print(e)
+                    print("RDS:", rds, "is not a valid coordinate pair")
+                    pass
+                # except Exception as e:
+                #     print(e)
 
             pygame.display.flip()
             clock.tick(60)
-            sleep(0.05)
+            # sleep(0.05)
                 
     except KeyboardInterrupt:
         print("Exiting program")
@@ -92,3 +161,81 @@ if __name__ == "__main__":
     s1[0] = "100 100 p".encode('utf-8')
     communication_simulator = CommunicationSimulator(drop_rate=0.01, bitflip_rate=0.01)
     rx_drawing(shared_input_buffer_name=s1.shm.name, communications_simulator=communication_simulator)
+
+
+    # def si4703getRDSBytes(self):        
+    #     z = "000000000000000"
+    #     msg = {}
+    #     mi = 0
+    #     h2 = ""
+    #     h3 = ""
+    #     h4 = ""
+    #     wc = 0
+    #     RDSB = self.SI4703_RDSB
+    #     RDSC = self.SI4703_RDSC
+    #     RDSD = self.SI4703_RDSD
+    #     reset = False
+    #     while 1:
+    #         if reset:
+    #             msg = {}
+    #             mi = 0
+    #             h2 = ""
+    #             h3 = ""
+    #             h4 = ""
+    #             wc = 0
+    #             reset = False
+
+    #         self.si4703ReadRegisters()
+    #         reg = self.si4703_registers
+    #         if reg[self.SI4703_STATUSRSSI] & (1 << 15):
+    #             r2 = z[:16 - len(bin(reg[RDSB])[2:])] + bin(reg[RDSB])[2:]
+    #             r3 = z[:16 - len(bin(reg[RDSC])[2:])] + bin(reg[RDSC])[2:]
+    #             r4 = z[:16 - len(bin(reg[RDSD])[2:])] + bin(reg[RDSD])[2:]
+    #             if h2 != r2 or h3 != r3 or h4 != r4:
+    #                 wc += 1
+    #                 h2 = r2
+    #                 h3 = r3
+    #                 h4 = r4
+    #                 value = int(r2[:4],2)
+    #                 value2 = int(r2[5:-5],2)
+    #                 if value2 == 0:
+    #                     type = "A"
+    #                 else:
+    #                     type = "B"
+    #                 code =  str(value) + type
+    #                 # print("Code", code)
+    #                 if code == "2B" or code == "2A":
+    #                     # chars = [bitstring_to_bytes(r3[:8]), bitstring_to_bytes(r3[9:]), bitstring_to_bytes(r4[:8]), bitstring_to_bytes(r4[9:])]
+    #                     # chars = [int(r3[:8], 2), int(r3[9:], 2), int(r4[:8], 2), int(r4[9:], 2)]
+
+    #                     string_chars = [r3[:8], r3[8:], r4[:8], r4[8:]]
+    #                     print('r3', r3, 'r4', r4)
+    #                     print('string_chars:', string_chars)
+
+    #                     chars = []
+    #                     for c in string_chars:
+    #                         chars.append(int(c, 2))
+
+    #                     index = int(r2[12:],2)
+    #                     print(index, chars)
+    #                     # print(str(index) + '-' +  str(chars))
+                        
+    #                     if index == 0 and mi != 0:
+    #                         # return ''.join(dict(sorted(msg.items())).values())
+    #                         dict_values = dict(sorted(msg.items())).values()
+    #                         # print('dict_values:', dict_values)
+    #                         byte_list = []
+    #                         for msg in dict_values:
+    #                             for entry in msg:
+    #                                 byte_list.append(entry)
+    #                         print('byte_list:', byte_list)
+    #                         return bytearray(byte_list)
+    #                         # print()
+    #                         # print("RDS MSG = " + ''.join(dict(sorted(msg.items())).values()))
+    #                         # print()
+    #                         # reset = True
+    #                         # break
+    #                     msg[index] = chars
+    #                     if index == mi:
+    #                         # msg += chars
+    #                         mi += 1
