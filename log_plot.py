f185cd05208b4369f0afc2426b511abca334f644
Alexander Puckhaber <alexanderpuckhaber@gatech.edu>
2023-12-02 15:20:53 -0500

    handle datetime strings with missing microseconds (was a whole number i guess)

--- log_plot.py
+++ log_plot.py
@@ -6,8 +6,13 @@ import os
 
 cwd = os.getcwd()
 # rx_logfile_name = 'logs_rx/2023-11-29 01:55:41.005135-rx.log'
-rx_logfile_name = os.path.join('logs_rx', '2023-11-29 17:56:26.168711-rx_1m_50ms.log')
-tx_logfile_name = os.path.join('logs_tx', '2023-11-29 17:56:33.472757-tx_1m_50ms.log')
+# rx_logfile_name = os.path.join('logs_rx', '2023-11-29 17:56:26.168711-rx_1m_50ms.log')
+# tx_logfile_name = os.path.join('logs_tx', '2023-11-29 17:56:33.472757-tx_1m_50ms.log')
+
+rx_logfile_name = os.path.join('logs_rx', '2023-11-29 18:34:28.631674-rx_1m_200ms.log')
+tx_logfile_name = os.path.join('logs_tx', '2023-11-29 18:34:30.524326-tx_1m_200ms.log')
+
+
 
 rx_log_rec = open(os.path.join(cwd, rx_logfile_name), 'r')
 tx_log_rec = open(os.path.join(cwd, tx_logfile_name), 'r')
@@ -36,15 +41,20 @@ def parse_log_file(log_file, is_tx=False):
         bin_str = obj['binary']
         int_val = int(bin_str[2:-1],16)
 
+        date_string = obj['datetime']
+
+        if '.' not in date_string:
+            date_string = date_string + '.0'
+
+        date_time_object = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S.%f')
         data.append(int_val)
-
-        date_time_object = datetime.strptime(obj['datetime'], '%Y-%m-%d %H:%M:%S.%f')
-
         time_val.append(date_time_object.timestamp())
+
     
     return time_val, data
 
 rx_time_val, rx_data = parse_log_file(rx_log_rec)
+print(len(rx_time_val), len(rx_data))
 tx_time_val, tx_data = parse_log_file(tx_log_rec, is_tx=True)
 
 plt.scatter(rx_time_val, rx_data, marker='o', s=1)
